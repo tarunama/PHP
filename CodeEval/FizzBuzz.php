04a54950638c39e1215e00dda1122b2de103a17d
sakeharami <tarunama@users.noreply.github.com>
2014-05-03 18:30:53 +0900

    refactoring score 25/100

--- CodeEval/FizzBuzz.php
+++ CodeEval/FizzBuzz.php
@@ -1,26 +1,23 @@
 <?php
 /*
 question:https://www.codeeval.com/open_challenges/1/
-*/	
+*/
 $fcontents = fopen($argv[1], "r");
-
-while( ($input_lines = fgets($fcontents)) !== false) {
+while(($input_lines = fgets($fcontents)) ==! false) {
     $ary = array();
     $ary = preg_split("[\s]",$input_lines);
-
-    for ($i = 1; $i < $ary[2] + 1; $i++){
+    
+    for ($i = 1; $i < $ary[2] + 1; $i++) {
         $f = $i % $ary[0];
         $b = $i % $ary[1];
 
-        if($f == 0){
+        if ($f == 0) {
             echo "F ";
             $i++;
-        }
-        elseif($b == 0){
+        } elseif ($b == 0) {
             echo "B ";
             $i++;
-        }        
-        if($f == 0 && $b == 0){
+        } elseif ($f == 0 && $b == 0) {
             echo "FB ";
             $i++;
         }
