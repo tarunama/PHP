b2fd2a29b70585cf00a6d44ea6a8fc3faa678458
meganeski <sakeharami@gmail.com>
2014-10-21 20:08:16 +0900

    totyu

--- CodeEval/FizzBuzz.php
+++ CodeEval/FizzBuzz.php
@@ -2,14 +2,17 @@
 /*
 question:https://www.codeeval.com/open_challenges/1/
 */
-$fcontents = fopen($argv[1], "r");
-while(($input_lines = fgets($fcontents)) ==! false) {
-    $ary = array();
-    $ary = preg_split("[\s]",$input_lines);
+
+$fcontents = fopen($argv[1], 'r');
+
+while (($input_lines = fgets($fcontents)) !== false) {
+    //$ary = preg_split('[\s]', trim($input_lines));
+    //$ary = explode(' ', trim($input_lines));
+    list($fst_int, $sec_int, $int_length) = explode(' ', trim($input_lines));
     
-    for ($i = 1; $i < $ary[2] + 1; $i++) {
-        $f = $i % $ary[0];
-        $b = $i % $ary[1];
+    for ($i = 1; $i < $int_length + 1; $i++) {
+        $f = $i % $fst_int;
+        $b = $i % $sec_int;
 
         if ($f == 0) {
             echo "F ";
