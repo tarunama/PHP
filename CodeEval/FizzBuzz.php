396ec71a4a1fd74c1d864865dc4349631654838e
meganeski <sakeharami@gmail.com>
2014-10-21 22:24:27 +0900

    refactoring
    add list
    valname

--- CodeEval/FizzBuzz.php
+++ CodeEval/FizzBuzz.php
@@ -6,25 +6,26 @@ question:https://www.codeeval.com/open_challenges/1/
 $fcontents = fopen($argv[1], 'r');
 
 while (($input_lines = fgets($fcontents)) !== false) {
-    //$ary = preg_split('[\s]', trim($input_lines));
-    //$ary = explode(' ', trim($input_lines));
-    list($fst_int, $sec_int, $int_length) = explode(' ', trim($input_lines));
+
+    list($fst_int, $sec_int, $int_num) = explode(' ', trim($input_lines));
     
-    for ($i = 1; $i < $int_length + 1; $i++) {
+    for ($i = 1; $i < $int_num + 1; $i++) {
         $f = $i % $fst_int;
         $b = $i % $sec_int;
 
-        if ($f == 0) {
-            echo "F ";
-            $i++;
-        } elseif ($b == 0) {
-            echo "B ";
-            $i++;
-        } elseif ($f == 0 && $b == 0) {
-            echo "FB ";
-            $i++;
+        if (($f === 0) && ($b === 0)) {
+            echo 'FB ';
+            continue;
+        } elseif ($f === 0) {
+            echo 'F ';
+            continue;
+        } elseif ($b === 0) {
+            echo 'B ';
+            continue;
+        } else {
+            echo $i . ' ';   
         }
-        echo $i . " ";
     }
-    echo "\n";
+    echo PHP_EOL;
 }
+fclose($fcontents);
