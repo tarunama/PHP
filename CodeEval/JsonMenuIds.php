618a350448070f408f740b2d49073f600660b5ca
meganeski <sakeharami@gmail.com>
2014-10-25 00:01:34 +0900

    refacrored

--- CodeEval/JsonMenuIds.php
+++ CodeEval/JsonMenuIds.php
@@ -1,21 +1,20 @@
 <?php
 
-$fh = fopen($argv[1], "r");
+$fh = fopen($argv[1], 'r');
 
 while ($line = fgets($fh)) {
     $ary    = array();
-    $ary[]  = json_decode($line, true);
+    $ary[]  = json_decode(trim($line), true);
     $sum    = 0;
+    $limit  = count($ary[0]['menu']['items']);
     
-    for ($i = 0; $i < count($ary[0]["menu"]["items"]); $i++) {
+    for ($i = 0; $i < $limit; $i++) {
         //値がNULLかどうかチェック
-        if (!isset($ary[0]["menu"]["items"][$i])){ continue; }
-        if (array_key_exists("label", $ary[0]["menu"]["items"][$i])) {
-            $sum = $sum + $ary[0]["menu"]["items"][$i]["id"];
+        if (!isset($ary[0]['menu']['items'][$i])){ continue; }
+        
+        if (array_key_exists('label', $ary[0]['menu']['items'][$i])) {
+            $sum += $ary[0]['menu']['items'][$i]['id'];
         }
     }
-    
-    echo $sum . "\n";
+    echo $sum . PHP_EOL;
 }
-
-fclose($fh););
