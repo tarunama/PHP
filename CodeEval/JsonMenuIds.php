1f5d86b4dbfc4b7cc39da12158bf5597f44bbbce
meganeski <sakeharami@gmail.com>
2014-11-04 21:32:11 +0900

    refactoring
    remove array define

--- CodeEval/JsonMenuIds.php
+++ CodeEval/JsonMenuIds.php
@@ -1,19 +1,16 @@
 <?php
-
 $fh = fopen($argv[1], 'r');
 
 while ($line = fgets($fh)) {
-    $ary    = array();
-    $ary[]  = json_decode(trim($line), true);
+    $ary    = json_decode(trim($line), true);
     $sum    = 0;
-    $limit  = count($ary[0]['menu']['items']);
+    $limit  = count($ary['menu']['items']);
     
     for ($i = 0; $i < $limit; $i++) {
-        //値がNULLかどうかチェック
-        if (!isset($ary[0]['menu']['items'][$i])){ continue; }
+        if (!isset($ary['menu']['items'][$i])){ continue; }
         
-        if (array_key_exists('label', $ary[0]['menu']['items'][$i])) {
-            $sum += $ary[0]['menu']['items'][$i]['id'];
+        if (array_key_exists('label', $ary['menu']['items'][$i])) {
+            $sum += $ary['menu']['items'][$i]['id'];
         }
     }
     echo $sum . PHP_EOL;
