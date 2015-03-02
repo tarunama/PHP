b1cdf6b027d4f97e47f241abaca5fc60040ce4ca
meganeski <sakeharami@gmail.com>
2015-03-02 23:36:06 +0900

    process wraped by class

--- CodeEval/FizzBuzz.php
+++ CodeEval/FizzBuzz.php
@@ -1,34 +1,43 @@
 <?php
+
+Class FizzBuzz
+{
+    private $fst_int;
+    private $scd_int;
+    private $times;
+
+    public function __construct() {
+        $this->scd_int   = $scd_int;
+        $this->fst_int = $fst_int;
+        $this->times = $times;
+    }
+
+    public function judgeFizzBuzz($n, $f_int, $s_int) {
+        if ($n % $f_int === 0 && $n % $s_int === 0) {
+            return 'FB ';
+        } elseif($n % $f_int === 0) {
+            return 'F ';
+        } elseif ($n % $s_int === 0) {
+            return 'B ';
+        } else {
+            return $n . ' ';
+        }
+    }
+
+    public function fizzBuzz($fst_int, $scd_int, $times) {
+        $result = '';
+        for ($i = 1; $i <= $times; $i++) {
+            $result .= $this->judgeFizzBuzz($i, $fst_int, $scd_int);
+        }
+        return $result;
+    }
+}
+
 $fcontents = fopen($argv[1], 'r');
 
-function judgeFizzBuzz($n, $fizz, $buzz)
-{
-    if ($fizz($n) && $buzz($n)) {
-        return 'FB ';
-    } elseif ($fizz($n)) {
-        return 'F ';
-    } elseif ($buzz($n)) {
-        return 'B ';
-    } else {
-        return $n . ' ';
-    }
-}
-
-function FizzBuzz($fst_int, $sec_int, $times)
-{
-    # conditions
-    $fizz = function($n) use ($fst_int) { return ($n % $fst_int === 0); };
-    $buzz = function($n) use ($sec_int) { return ($n % $sec_int === 0); };
-
-    # poor
-    $result = '';
-    for ($i = 1; $i <= $times; $i++) {
-        $result .= judgeFizzBuzz($i, $fizz, $buzz);
-    }
-    return $result;
-}
+$fb = new FizzBuzz();
 
 while ( ($input_lines = fgets($fcontents)) ) {
-    list($fst_int, $sec_int, $times) = explode(' ', trim($input_lines));
-    echo FizzBuzz($fst_int, $sec_int, $times) . PHP_EOL;
+    list($fst_int, $scd_int, $times) = explode(' ', trim($input_lines));
+    echo $fb->fizzBuzz($fst_int, $scd_int, $times) . PHP_EOL;
 }
