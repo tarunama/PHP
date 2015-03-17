4491dca6a996a5a42bb2077b306fc9425239ab97
meganeski <sakeharami@gmail.com>
2015-03-18 07:08:46 +0900

    modify OOP

--- CodeEval/JsonMenuIds.php
+++ CodeEval/JsonMenuIds.php
@@ -1,17 +1,36 @@
 <?php
 $fh = fopen($argv[1], 'r');
 
-while ($line = fgets($fh)) {
-    $ary    = json_decode(trim($line), true);
-    $sum    = 0;
-    $limit  = count($ary['menu']['items']);
-    
-    for ($i = 0; $i < $limit; $i++) {
-        if (!isset($ary['menu']['items'][$i])){ continue; }
-        
-        if (array_key_exists('label', $ary['menu']['items'][$i])) {
-            $sum += $ary['menu']['items'][$i]['id'];
+Class JsonMenuIds
+{
+    public getItemLimit ($ary) {
+        return count($ary['menu']['items']);
+    }
+
+    public validateExistItem ($ary) {
+        if (!isset($ary['menu']['items'][$i])){
+          return continue;
         }
+        return;
+    }
+
+    public countItems ($ary) {
+        if ( array_key_exists('label', $ary['menu']['items'][$i]) ) {
+            return $ary['menu']['items'][$i]['id'];
+        }
+    }
+}
+
+$jmi = new JsonMenuIds();
+
+while ($line = fgets($fh)) {
+    $sum    = 0;
+    $ary    = json_decode(trim($line), true);
+    $limit  = $jmi->getItemLimit($ary);
+
+    for ($i = 0; $i < $limit; $i++) {
+        $jmi->validateExistItem($ary);
+        $sum += $jmi->countItems($ary);
     }
     echo $sum . PHP_EOL;
 }
